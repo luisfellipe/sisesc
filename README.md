5c903f3bfc65f6053f15d9ae39fddc8c353a62c7
Luís Felipe <felipealvesoares@gmail.com>
2019-11-20 20:01:25 -0300

    alterações no README.md e inserções de novos arquivos

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# Trabalho de Engenharia de Software
-
-** Sisesc Sistema Escolar para Matricula **
-
-<table>
-	<hd>
-		
-	</hd>
-
-</table>
+# Sisesc -  Sistema para Gerenciamento de Matriculas Escolares .
+### Trabalho de Engenharia de Software
+<hr>
+####Ferramentas
+- Cadastro de Alunos
+- Emissão de Documentos Ex. Histórico, Certificados etc.
+- Gerenciamento de Turmas
+- Calendário Escolar
+- Lançamentos de notas e presença
+- Comunicados importantes
+<hr>
+###IFSULDEMINAS - Campus Passos MG
+**Cuso:** Ciência da Computação
+**Prof:** Vinícius Silva
+**Alunos:** Erik Fonseca e Luís Felipe
---
+++ php/paginas/aluno/cad-aluno.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tela do Funcionário</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="../../css/colorfont.css">
+		<link rel="stylesheet" type="text/css" href="../../css/estilo.css">
+		<link rel="stylesheet" type="text/css" href="../../css/tela.css">
+		<script type="text/javascript" src="../../js/submenus.js"></script>
+	</head>
+	<body>	
+			<header id="cabecalho">
+				<div>
+					<img src="#" width="250px" height="100px">
+				</div>
+			</header>
+
+			<aside id="menu">
+				<ul>
+					<li onclick="showMenu('subAlunos')">Alunos</li>
+						<ul class="sub" id="subAlunos">
+							<li>Cadastrar</li>
+							<li>Consulta/Alterar</li>
+							<li>Boletim</li>
+						</ul>
+					<li>Cursos</li>
+					<li onclick="showMenu('subTurma')">Turma</li>
+						<ul class="sub" id="subTurma">
+							<li>Diário</li>
+							<li>Notas</li>
+						</ul>
+					<li>Calendário</li>
+					<li>Comunicados</li>			
+				</ul>
+			</aside>
+
+			<article id="corpo">
+				
+
+			</article>
+
+
+			<footer id="rodape">
+				
+			</footer>
+
+
+			<ul>
+				<li>Cadastro de Alunos</li>
+				<li>- Emissão de Documentos Ex. Histórico</li>
+				<li>- Gerenciamento de Turmas</li>
+				<li>- Calendário Escolar</li>
+				<li>- Lançamentos de notas e presença</li>
+				<li>- Comunicados importantes</li>
+			</ul>
+	</body>
+</html>
